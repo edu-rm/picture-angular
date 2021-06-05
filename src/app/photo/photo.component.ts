5f33364d09a876baee31ec1391a77cb56b19e87d
edu-rm <eduardomeireles3232@gmail.com>
2021-06-05 18:28:02 -0300

    @input decorator: inbounding properties

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "pm-photo",
   templateUrl: "./photo.component.html",
 })
 export class PhotoComponent {
-  description = "HueHueBr";
-  url =
-    "https://thumbs.jusbr.com/imgs.jusbr.com/publications/artigos/144922528/images/1412967253.jpeg";
+  @Input() description = "";
+  @Input() url = "";
 }
