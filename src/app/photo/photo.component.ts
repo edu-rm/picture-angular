224f1ae79e0a521e9caeec24c85bc0352acf3b14
edu-rm <eduardomeireles3232@gmail.com>
2021-06-05 18:21:11 -0300

    photo component + module concept

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "pm-photo",
+  templateUrl: "./photo.component.html",
+})
+export class PhotoComponent {
+  description = "HueHueBr";
+  url =
+    "https://thumbs.jusbr.com/imgs.jusbr.com/publications/artigos/144922528/images/1412967253.jpeg";
+}
