e586a64945f7363904b1aca05fa648e4c5b9d0c4
edu-rm <eduardomeireles3232@gmail.com>
2021-06-06 20:02:29 -0300

    routing not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModules } from "./app.routing.module";
 import { PhotosModule } from "./photos/photos.module";
+import { ErrorsModule } from "./errors/errors.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModules, PhotosModule],
+  imports: [BrowserModule, AppRoutingModules, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
